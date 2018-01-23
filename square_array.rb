9adc99e99d036fd8d7112ea4cd5afed7076443d0
Stacey  Marks <staceyamarks@gmail.com>
2018-01-23 14:42:03 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,8 @@
-# def square_array(array)
-#   new_array = []
-#   for value in array
-#     new_array << value ** 2
-#   end
-#   new_array
-# end
-
 def square_array(array)
-  array.collect {|x| x**2}
+  new_array = []
+  for value in array
+    new_array << value ** 2
+  end
+  new_array
 end
+
